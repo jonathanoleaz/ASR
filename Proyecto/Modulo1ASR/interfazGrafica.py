10322ea7fa847e8a954c89ef0e7c39aa15a523bf
Cesar Cruz <cesar.cruz182@gmail.com>
2018-10-14 12:23:44 -0500

    Interfaz grafica completa. Falta back de la prediccion

--- Proyecto/Modulo1ASR/interfazGrafica.py
+++ Proyecto/Modulo1ASR/interfazGrafica.py
@@ -198,6 +198,10 @@ def espacioEntreComponentes(root):
   espacio = Label(root, text="\n")
   espacio.pack()
 
+def insertarComponentesEnVentana(componentes):
+  for componente in componentes:
+    componente.pack()
+
 def seleccionarFechas():
   # Metodo para seleccionar las fechas de la prediccion
   l_inicio = Label(root, text="Seleccione fecha de inicio")
@@ -205,27 +209,43 @@ def seleccionarFechas():
   l_horaI = Label(root, text="Seleccione la hora")  
   hora_inicio = Entry(root)
   hora_inicio.insert(END, '00:00')
-  l_inicio.pack()
-  cal_inicio.pack()
-  l_horaI.pack()
-  hora_inicio.pack()
-
+  insertarComponentesEnVentana([l_inicio, cal_inicio, l_horaI, hora_inicio])
+  
   espacioEntreComponentes(root)
   l_final = Label(root, text="Seleccione fecha de termino")
   cal_final = Calendar(root, font="Arial 12", year=2018, month=10, day=2)
   l_horaF = Label(root, text="Seleccione la hora")
   hora_final = Entry(root)
   hora_final.insert(END, '23:59')
-  l_final.pack()
-  cal_final.pack()
-  l_horaF.pack()
-  hora_final.pack()
-
+  insertarComponentesEnVentana([l_final, cal_final, l_horaF, hora_final])
+  
   # Boton para realizar las predicciones  
   espacioEntreComponentes(root)
-  boton_predecir = Button(root, text="Predecir", command= lambda: realizar_prediccion(\
+  boton_sig = Button(root, text="Siguiente", command= lambda: especificacionDatos(\
     cal_inicio.selection_get(), hora_inicio.get(), cal_final.selection_get(), hora_final.get()))
-  boton_predecir.pack()
+  boton_sig.pack()
+
+def especificacionDatos(fecha_inicio, hora_inicio, fecha_termino, hora_termino):
+  clearVentana()
+
+  # Informacion de la variable a realizar la prediccion
+  l_agente = Label(root, text="Nombre (ip / hostname) del agente:")
+  agente = Entry(root)
+  l_base = Label(root, text="Nombre de la base: ")
+  base = Entry(root)
+  l_variable = Label(root, text="Nombre de la variable: ")
+  variable = Entry(root)
+  l_min = Label(root, text="Limite inferior (-1 si no hay): ")
+  limInf = Entry(root)
+  l_max = Label(root, text="Limite superior (-1 si no hay): ")
+  limSup = Entry(root)
+
+  btn_predecir = Button(root, text="Predecir!", command= lambda: realizar_prediccion(\
+    fecha_inicio, hora_inicio, fecha_termino, hora_termino,\
+    agente.get(), base.get(), variable.get(), limInf.get(), limSup.get()))
+  
+  insertarComponentesEnVentana([l_agente, agente, l_base, base, l_variable, variable,\
+    l_min, limInf, l_max, limSup, btn_predecir])
 
 # Creacion de la ventana
 root = Tk()
--- Proyecto/Modulo1ASR/prediccion/predecir.py
+++ Proyecto/Modulo1ASR/prediccion/predecir.py
@@ -10,18 +10,28 @@ from time import mktime
 ONE_HR_POSIX = 86400 / 24
 ONE_MIN_POSIX = ONE_HR_POSIX / 60
 
-def realizar_prediccion(fecha_inicio, hora_inicio, fecha_termino, hora_termino):  
+def realizar_prediccion(fecha_inicio, hora_inicio, fecha_termino, hora_termino,\
+  agente, baseRRD, variable, limInf, limSup):  
+  posix_inicio = calculo_fecha_posix(fecha_inicio, hora_inicio)
+  posix_termino = calculo_fecha_posix(fecha_termino, hora_termino)
+  """
   posix_fecha_inicio = mktime(fecha_inicio.timetuple())  
   hr_inicio, min_inicio = hora_inicio.split(":")
   h_inicio = (ONE_HR_POSIX * float(hr_inicio)) + (ONE_MIN_POSIX * float(min_inicio))
   posix_inicio = posix_fecha_inicio + h_inicio  
-
+  
   # Calculo de posix_termino
   posix_fecha_termino = mktime(fecha_termino.timetuple())
   hr_termino, min_termino = hora_termino.split(":")
   h_termino = (ONE_HR_POSIX * float(hr_termino)) + (ONE_MIN_POSIX * float(min_termino))
   posix_termino = posix_fecha_termino + h_termino 
-
+  """
   print 'Hr inicial: -> ' + str(posix_inicio)
   print 'Hr termino: -> ' + str(posix_termino)
+
+def calculo_fecha_posix(fecha, hora):
+  posix_fecha = mktime(fecha.timetuple())
+  hr, mins = hora.split(":")
+  posix_hora = (ONE_HR_POSIX * float(hr)) + (ONE_MIN_POSIX * float(mins))
+  return posix_fecha + posix_hora 
   
